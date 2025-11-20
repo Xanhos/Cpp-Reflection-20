ae5c71150d5d121c50f5734d30630af503e2b62f
Yann Grallan <grallanyann84@gmail.com>
2025-11-20 11:36:50 +0100

    Add enum reflection support and test
    Introduces REFLECT_ENUM and ENUM_FIELD macros for enum reflection in Reflection.h, adds EnumInfo and related utilities, and provides GetEnumName and GetEnumValue functions. Updates CMakeLists.txt to build a Reflection_Test executable and adds test.cpp to demonstrate enum reflection usage.

---
+++ test.cpp
@@ -0,0 +1,22 @@
+﻿#include "Reflection.h"
+
+enum class Test
+{
+    a,b,c = 454156
+};
+
+REFLECT_ENUM(Test,
+    ENUM_FIELD(Test::a),
+    ENUM_FIELD(Test::b),
+    ENUM_FIELD(Test::c)
+    );
+
+int main()
+{
+    auto t = Reflection::EnumInfo<Test>::fields;
+
+    auto EnumName = Reflection::GetEnumName(Test::c);
+    std::cout <<  EnumName << "\n";
+    std::cout << (int)Reflection::GetEnumValue<Test>(EnumName) << "\n";
+
+}
